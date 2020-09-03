b650e90c68836ebf9261d31bc6c89b2eda809b88
ipetrovski3 <ipetrovski3@gmail.com>
2020-09-03 14:05:43 +0200

    added tailwind css

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
-module.exports = {
+let enviroment = {
   plugins: [
+    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -10,3 +11,16 @@ module.exports = {
     })
   ]
 }
+
+if (process.env.RAILS_ENV === 'production') {
+  enviroment.plugins.push
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/.html.erb',
+        './app/**/*.rb',
+        './app/javascript/*.js',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+}
+module.exports = enviroment;
