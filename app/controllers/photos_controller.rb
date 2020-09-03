0f2395b4876fd3649caf95acd20106708f89ae1d
ipetrovski3 <ipetrovski3@gmail.com>
2020-09-03 14:27:07 +0200

    photos controller implemented

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,25 @@
+class PhotosController < ApplicationController
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+
+    if @photo.save
+      redirect_to @photo
+    else
+      render :new
+    end
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:description, :image)
+  end
+end
