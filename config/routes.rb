5f18080f29920e5ec6f6df1005e7161e061857f5
ipetrovski3 <ipetrovski3@gmail.com>
2020-09-04 11:13:14 +0200

    minor restiling in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pages#index'
+  root 'photos#index'
 
   resources :photos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
